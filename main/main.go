a6170d649cdb46ea06b367c0335a86c44af32c32
yangshujie <yangshujie@fangcunyisheng.com>
2023-02-19 14:07:34 +0800

    feat 更新目录结构

--- main/main.go
+++ main/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"GoLearning/case/hello"
-	"GoLearning/case/pointer"
-	hungrySingleton "GoLearning/case/singleton/hungry"
 	"fmt"
 )
 
@@ -11,29 +8,8 @@ func main() {
 
 	printTest()
 
-	helloWorldTest()
-
 }
 
 func printTest() {
 	fmt.Println("test print something. main package needs to import fmt package, event if the fmt package has already imported by hello package")
-}
-
-// 测试单例模式
-func singletonTest() {
-	var person = hungrySingleton.GetInsOr()
-
-	person.Greet()
-}
-
-// 跑通 hello world
-func helloWorldTest() {
-	hello.Hello()
-}
-
-// 测试 Go 语言指针类型
-func pointerTest() {
-	pointer.Test1()
-
-	pointer.Test2()
 }
