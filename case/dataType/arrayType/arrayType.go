bd95813dc84331a3cc5788e35e170043d599246e
yangshujie <yangshujie@fangcunyisheng.com>
2023-03-09 13:17:54 +0800

    feat 测试 package 内可见的数组变量

--- case/dataType/arrayType/arrayType.go
+++ case/dataType/arrayType/arrayType.go
@@ -6,16 +6,22 @@ var PublicArr = [...]int{1, 2, 3}
 var PublicArr2 = [...]int{}
 
 // package 内可见
-var arr0 [5]int = [5]int{1, 2, 3}
-var arr1 = [5]int{1, 2, 3, 4, 5}
-var arr2 = [...]int{1, 2, 3, 4, 5, 6}
+var arr0 [5]int
+var arr1 [5]int = [5]int{1, 2, 3}
+var arr2 = [5]int{1, 2, 3, 4, 5}
+var arr3 = [...]int{1, 2, 3, 4, 5, 6}
 var strArr = [5]string{3: "hello", 4: "world"}
 
 func Test1() {
-	fmt.Println(arr0)
-	fmt.Println(arr1)
-	fmt.Println(arr2)
-	fmt.Println(strArr)
+	fmt.Println("arr0: ", arr0)
+	arr0[0] = 3
+	arr0[1] = 2
+	arr0[2] = 1
+	fmt.Println("arr0: ", arr0)
+
+	fmt.Println("arr1: ", arr1)
+	fmt.Println("arr2: ", arr2)
+	fmt.Println("strArr: ", strArr)
 
 	PublicArr[0] = 3
 }
